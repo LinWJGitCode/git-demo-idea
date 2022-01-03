c305427b51adef5a83aaed04827b6db0cd0f1cf0
lwj <865020756@qq.com>
2022-01-04 01:25:14 +0800

    first commit

---
+++ src/main/java/com/lin/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.lin.git;
+
+/**
+ * @author lwj
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+
+    }
+}
